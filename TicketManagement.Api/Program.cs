41e0282f8319dbcabd407b9084bc87658c920369
Can Burak Türkmen <canburak.turkmen1@gmail.com>
2023-06-30 17:34:04 +0300

    add logging

--- TicketManagement.Api/Program.cs
+++ TicketManagement.Api/Program.cs
@@ -1,10 +1,24 @@
 using TicketManagement.Api;
 
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("GloboTicket API starting");
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration
+     .WriteTo.Console()
+     .ReadFrom.Configuration(context.Configuration));
 
 var app = builder
     .ConfigureServices()
     .ConfigurePipeline();
+
+app.UseSerilogRequestLogging();
 
 if (builder.Environment.IsDevelopment())
 {
